ea9f1ba33b9dd1453994d11aab6ccf83b07c153c
Barbara Stelmaszek <102234080+barbs81@users.noreply.github.com>
2024-01-19 11:17:35 +0100

    Update GameMainApplication.java
    same

--- src/main/java/com/monopoly/monopoly/GameMainApplication.java
+++ src/main/java/com/monopoly/monopoly/GameMainApplication.java
@@ -2,19 +2,32 @@ package com.monopoly.monopoly;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import org.json.JSONArray;
+
 import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class GameMainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(GameMainApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-//        GameManager gm = new GameManager();
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("start-window.fxml"));
+            Image icon = new Image(getClass().getResourceAsStream("monopolyIcon.png"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle("MONOPOLY");
+            stage.getIcons().add(icon);
+            stage.show();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 
     public static void main(String[] args) {
